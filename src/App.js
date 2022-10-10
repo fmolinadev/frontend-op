b63e91676b55e12785f065757d36cd863fed9b84
Francisco Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-10-10 17:36:48 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div >
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+export default App;
