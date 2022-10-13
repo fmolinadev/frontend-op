25f6f083be87b05851ccce9a67107edc0015ebfd
Francisco Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-10-13 00:23:46 -0300

    feat(task list): agregue la tabla para renderizar cada tarea

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,16 +1,44 @@
+import { useState } from "react"
 import TaskComponent from "../pure/task"
 import { Task } from "../../models/task.class"
 import { PRIORITY } from "../../models/priority.enum"
 
 const TasklistComponent = () => {
 
-    const defaulTask = new Task("Estudiar", "Repasar conceptos de React en Open Bootcamp", "false", PRIORITY.LOW)
+
+    const defaulTask = new Task("Estudiar", "Repasar conceptos de React en Open Bootcamp", false, PRIORITY.LOW)
+    const defaulTask1 = new Task("Correr", "Salir a trotar al parque 1 hora", false, PRIORITY.MID)
+    const defaulTask2 = new Task("Cocinar", "Hacer el almuerzo", true, PRIORITY.HIGHT)
+    const [tasks, /* setTask */] = useState([defaulTask, defaulTask1, defaulTask2])
+
+    const completeTask = () => {
+        console.log("Tarea completada")
+    }
+
 
     return (
         <div>
-            <div>
-                <h1>Tus tareas:</h1>
-                <TaskComponent task={defaulTask} />
+            <div class="container mx-auto px-9">
+                <div>
+                    <h1 class="font-semibold text-2xl">Tus tareas:</h1>
+                </div>
+                <div >
+                    <table>
+                        <thead class="text-center ">
+                            <tr class="columns-4 m-3 justify-center" >
+                                <th scope="col" class="w-50 h-10">Titulo</th>
+                                <th scope="col" class="w-96 h-10">Descripcion</th>
+                                <th scope="col" class="w-50 h-10 px-7">Prioridad</th>
+                                <th scope="col" class="w-50 h-10">Estado</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                tasks.map((task, index) => <TaskComponent key={index} task={task} complete={completeTask} />)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
