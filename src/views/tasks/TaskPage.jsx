950979d9c82d1d4ea402e08922cb6e6aa28c77f8
Francisco Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-10-15 20:17:22 -0300

    feat(views-task): agregue la vista principal de todas las tareas

---
+++ src/views/tasks/TaskPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TasklistComponent from "../../components/container/task_list";
+
+const TaskPage = () => {
+  return (
+    <div>
+      <TasklistComponent />
+    </div>
+  );
+};
+
+export default TaskPage;
