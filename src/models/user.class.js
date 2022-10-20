037a1078776d3f3eb887dba92361c6e0f2760871
Francisco Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-10-20 16:25:05 -0300

    fix(user): edite el modelo para agregar mail

--- src/models/user.class.js
+++ src/models/user.class.js
@@ -3,12 +3,14 @@ import { ROL } from "./roles.enum";
 export class User {
   userName = "";
   userSurname = "";
+  userEmail = "";
   userPassword = "";
   rol = ROL.USER;
 
-  constructor(userName, userSurname, userPassword, rol) {
+  constructor(userName, userSurname, userEmail, userPassword, rol) {
     this.userName = userName;
     this.userSurname = userSurname;
+    this.userEmail = userEmail;
     this.userPassword = userPassword;
     this.rol = rol;
   }
